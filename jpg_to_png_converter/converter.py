ee8d5a349fe09ea0999bcd73b73cbb385f59e3ed
alditus <alvarez.aldoandres@gmail.com>
2024-02-19 14:00:59 -0300

    Added necesary code to convert images from input folder to png and save them to output dir

--- jpg_to_png_converter/converter.py
+++ jpg_to_png_converter/converter.py
@@ -1,19 +1,41 @@
 import os
+from PIL import Image
 
-def check_and_create_utility_folders():
-	current_working_directory = os.path.abspath(os.getcwd())
-	output_dir = os.path.join(current_working_directory, 'output')
-	input_dir = os.path.join(current_working_directory, 'input')
+CURRENT_WORKING_DIRECTORY = os.path.abspath(os.getcwd())
+INPUT_DIR = os.path.join(CURRENT_WORKING_DIRECTORY, 'input')
+OUTPUT_DIR = os.path.join(CURRENT_WORKING_DIRECTORY, 'output')
 
+def check_if_input_folder_exists(input_dir):
+	if not os.path.isdir(input_dir):
+		print("Input folder does not exist, creating one...")
+		print("Please locate your images inside this folder.")
+		os.mkdir(input_dir)
+		
+
+def get_files_from_input_folder(input_dir):
+	img_files_names = os.listdir(input_dir)
+	img_files_paths = [os.path.join(input_dir, x) for x in img_files_names]
+	return img_files_paths
+
+def convert_to_png_and_save(image_path, output_dir):
+	head, tail = os.path.split(image_path)
+	img = Image.open(image_path)
+
+	# if not OUTPUT DIR create one
 	if not os.path.isdir(output_dir):
 		os.mkdir(output_dir)
 
-	if not os.path.isdir(input_dir):
-		os.mkdir(input_dir)
-	
-	print(os.path.isdir(output_dir))
-	print(os.path.isdir(input_dir))
-	print(current_working_directory)
+	output_file_name = '.'.join(tail.split('.')[:-1])
+	output_file_name = f'{output_file_name}.png'
+	img.save(os.path.join(output_dir, output_file_name), format="png")
+
+def run_converter(input_dir, output_dir):
+	check_if_input_folder_exists(input_dir)
+	images_paths = get_files_from_input_folder(input_dir)
+	for img_path in images_paths:
+		convert_to_png_and_save(img_path, output_dir)
 
 if __name__ == "__main__":
-	check_and_create_utility_folders()
+	run_converter(INPUT_DIR, OUTPUT_DIR)
+	
+	
