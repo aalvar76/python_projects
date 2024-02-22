16bf4bd6b82c288aa99995b23311c35370c91e29
alditus <alvarez.aldoandres@gmail.com>
2024-02-22 15:22:31 -0300

    Added contacts.txt and the necesary code to read contacts from file

--- email_sender/email_sender.py
+++ email_sender/email_sender.py
@@ -1,5 +1,8 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+import smtplib
+from dotenv import load_dotenv, find_dotenv # environs package is another option to use here
+from email.message import EmailMessage
+from pathlib import Path
 
 # Load environment variables from .env file
 load_dotenv(find_dotenv('.env.dev')) #  replace this '.env.dev' by the environmet file name
@@ -10,9 +13,11 @@ smtp_port = os.getenv("SMTP_PORT")
 email_address = os.getenv("EMAIL_ADDRESS")
 email_password = os.getenv("EMAIL_PASSWORD")
 
-# Print and check values from env file
+# Read the list of contacts to send email to
+contacts = []
+with open(Path('contacts.txt')) as file:
+	lines = file.readlines()
+	# for each line we have name and email separated by a comma
+	contacts = [tuple(x.split(',')) for x in lines]
 
-print(smtp_server)
-print(smtp_port)
-print(email_address)
-print(email_password)
+print(contacts)
